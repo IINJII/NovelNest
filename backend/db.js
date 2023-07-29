88019f3fc1237182b822659c23bcb5ec07568b7b
Nitish <111342262+IINJII@users.noreply.github.com>
2023-07-30 00:23:49 +0530

    Second Commit

--- backend/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const URI = 'mongodb://127.0.0.1:27017/BookStore'
-
-const mongoConnect = () => {
-    mongoose.connect(URI);
-}
-
-module.exports = mongoConnect;
